93b62f899925640478d4bcf6c7de9b191dbd1005
Patrick Grady <pmgrady2@illinois.edu>
2019-12-17 23:20:52 -0800

    length limit on generator examples

--- dataloader.py
+++ dataloader.py
@@ -52,8 +52,9 @@ class Dis_dataloader():
                 line = line.strip()
                 line = line.split()
                 parse_line = [int(x) for x in line]
-                if len(parse_line) == 20:
-                    negative_examples.append(parse_line)
+                # if len(parse_line) == 20:
+                negative_examples.append(parse_line)
+                
         self.sentences = np.array(positive_examples + negative_examples)
 
         # Generate labels
