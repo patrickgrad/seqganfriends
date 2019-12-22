a570ed724034bc849c6f486fca808081c884491c
Patrick Grady <pmgrady2@illinois.edu>
2019-12-22 16:11:35 -0500

    to py 3

--- discriminator.py
+++ discriminator.py
@@ -59,9 +59,9 @@ class Discriminator(object):
             self, sequence_length, num_classes, vocab_size,
             embedding_size, filter_sizes, num_filters, l2_reg_lambda=0.0):
         # Placeholders for input, output and dropout
-        self.input_x = tf.placeholder(tf.int32, [None, sequence_length], name="input_x")
-        self.input_y = tf.placeholder(tf.float32, [None, num_classes], name="input_y")
-        self.dropout_keep_prob = tf.placeholder(tf.float32, name="dropout_keep_prob")
+        self.input_x = tf.compat.v1.placeholder(tf.int32, [None, sequence_length], name="input_x")
+        self.input_y = tf.compat.v1.placeholder(tf.float32, [None, num_classes], name="input_y")
+        self.dropout_keep_prob = tf.compat.v1.placeholder(tf.float32, name="dropout_keep_prob")
 
         # Keeping track of l2 regularization loss (optional)
         l2_loss = tf.constant(0.0)
