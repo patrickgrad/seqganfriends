e221bb7b7c93c29c29524ede20089b361e55110e
Patrick Grady <pmgrady2@illinois.edu>
2019-12-28 00:41:37 -0500

    add execute script

---
+++ execute.sh
@@ -0,0 +1,5 @@
+python sequence_gan.py --character chandler
+python sequence_gan.py --character ross
+python sequence_gan.py --character monica
+python sequence_gan.py --character rachel
+python sequence_gan.py --character phoebe
